e13ba9c784562c045468b75bb4c38f098c474eb0
anasHammaoui <anashammaoui07@gmail.com>
2026-01-01 10:00:00 +0100

    initial Spring Boot project setup

---
+++ src/main/java/com/example/Taskora/TaskoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Taskora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Taskora/TaskoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taskora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
